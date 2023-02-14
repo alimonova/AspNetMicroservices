e03722cc34112a12935f58172866d93d41544e84
alimonova <sburchinskaya.2000@gmail.com>
2023-02-14 16:05:04 +0200

    Migrations was enabled with initial migration creating

--- src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -15,7 +15,7 @@ namespace Ordering.Application
 			services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 			services.AddMediatR(Assembly.GetExecutingAssembly());
 
-			services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
 			services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
             return services;
