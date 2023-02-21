65f513547f8c445cab8b3bc33dddff4085217904
alimonova <sburchinskaya.2000@gmail.com>
2023-02-21 22:16:09 +0200

    Ocelot json files were updated

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,20 +1,26 @@
 ﻿using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-Host.CreateDefaultBuilder(args)
-    .ConfigureLogging((hostingContext, loggingbuilder) =>
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+
+using var loggerFactory = LoggerFactory.Create(loggingBuilder =>
 {
-    loggingbuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-    loggingbuilder.AddConsole();
-    loggingbuilder.AddDebug();
+    loggingBuilder
+        .AddConfiguration(builder.Configuration.GetSection("Logging"))
+        .AddConsole()
+        .AddDebug();
 });
 
-builder.Services.AddOcelot();
+ILogger logger = loggerFactory.CreateLogger<Program>();
 
+builder.Services.AddOcelot()
+    .AddCacheManager(settings => settings.WithDictionaryHandle());
+var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
-
 await app.UseOcelot();
+
 app.Run();
+
